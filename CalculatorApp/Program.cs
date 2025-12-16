62502beb71d20b0ca1ed5f451bdda1e2be1e11c6
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-16 11:56:32 +0530

    Remove unrelated Prog1.cs file

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculatorApp;
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        Console.WriteLine("Add: " + calc.Add(10, 5));        // 15
+        Console.WriteLine("Subtract: " + calc.Subtract(10, 5)); // 5
+    }
+}
--- obj/Debug/net9.0/App.AssemblyInfo.cs
+++ obj/Debug/net9.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+939a454eb93f56d550e982d637116ce71d7dbd0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25fb3e762fa9c33c21ed42d8b6f906d0b74b9d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
