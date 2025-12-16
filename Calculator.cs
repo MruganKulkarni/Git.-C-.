eb97f5860afdda1b0100c369437fbb2ba09310dd
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-16 12:17:16 +0530

    Calculator prog added

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+           return a - b; 
+        }
+    }
+
+
+
+    
+}
