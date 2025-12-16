3659e9f2ccd2f6a68a3ba22bc0634882cb8296e5
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-16 15:57:12 +0530

    Password check code done

---
+++ BridgeLabz1/Program7_PasswordStrength/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        int score = 0;
+
+        if (password.Length >= 8)
+            score++;
+
+        bool hasDigit = false;
+        bool hasSpecial = false;
+
+        foreach (char c in password)
+        {
+            if (char.IsDigit(c))
+                hasDigit = true;
+            else if (!char.IsLetterOrDigit(c))
+                hasSpecial = true;
+        }
+
+        if (hasDigit)
+            score++;
+        if (hasSpecial)
+            score++;
+
+        if (score == 1)
+            Console.WriteLine("Weak");
+        else if (score == 2)
+            Console.WriteLine("Medium");
+        else if (score == 3)
+            Console.WriteLine("Strong");
+        else
+            Console.WriteLine("Very Weak");
+    }
+}
