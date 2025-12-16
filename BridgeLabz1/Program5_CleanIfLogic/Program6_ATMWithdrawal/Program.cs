91026d8bb7c314fcf94f8cbe436a0c81375d9561
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-16 15:32:56 +0530

    ATMWithdrawal Code done

---
+++ BridgeLabz1/Program5_CleanIfLogic/Program6_ATMWithdrawal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int balance = 30000;
+        int minimumBalance = 1000;
+        int dailyLimit = 20000;
+
+        Console.Write("Enter withdrawal amount: ");
+        int amount = int.Parse(Console.ReadLine());
+
+        if (amount <= dailyLimit)
+        {
+            if (amount <= balance - minimumBalance)
+            {
+                Console.WriteLine("Withdrawal successful");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance (minimum balance must be maintained)");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Amount exceeds daily withdrawal limit");
+        }
+    }
+}
