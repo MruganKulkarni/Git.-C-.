85ff69ee42403dbc9071d6e74d8068ed844d17c9
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-17 14:53:40 +0530

    commited

--- BridgeLabz2/Level1Problems/Program5_VotingEligibility/Program.cs
+++ BridgeLabz2/Level1Problems/Program5_VotingEligibility/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{    static void Main()
+    {
+        Console.Write("Enter age: ");
+        int age = int.Parse(Console.ReadLine());
+        if (age >= 18)
+        {
+            Console.WriteLine($"The person's age is {age} and can vote.");
+        }
+        else
+        {
+            Console.WriteLine($"The person's age is {age} and cannot vote.");
+        }
+    }
+}
