9e1eba11a778bd98b428557afcc65935fbd26cfc
MruganKulkarni <global-mrugan.kulkarni63@gmail.com>
2025-12-17 11:00:38 +0530

    Sample file added

---
+++ BridgeLabz2/Level1Problems/Program1_DivisibleBy5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class divbyfive
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        // Criteria to check if number is divisible
+        bool isDivisible = (num % 5 == 0);
+        string condition = isDivisible? "Yes" : "No";
+
+        Console.WriteLine($"Is the number {num} divisible by 5? {condition}");
+    }
+}
---
+++ BridgeLabz2/Level1Problems/Program3_LargestOfThree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number:");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.Write("Enter third number:");
+        int num3 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Is the first number the largest? {num1 > num2 && num1 > num3}");
+        Console.WriteLine($"Is the second number the largest? {num2 > num1 && num2 > num3}");
+        Console.WriteLine($"Is the third number the largest? {num3 > num1 && num3 > num2}");
+    }
+}
